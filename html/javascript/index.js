09f77119498c4546d3447733d7239ab64353e551
RainBell98 <ttiger45@naver.com>
2023-07-10 20:34:34 +0900

    js

---
+++ html/javascript/index.js
@@ -0,0 +1,52 @@
+// alert("링크방식")
+
+// // 키워드: var,let,const
+
+// var num = 5;
+
+// var num1;
+// num1 = 5;
+// num1 = 6;
+
+// let string = "가나다";
+// string = "라마바"
+// // let string = "사" :불가능
+
+// const string2 = "마마마"
+// string2 = "바바바"//오류
+
+
+
+// // var,let,const
+// // var: 재선언, 재할당 가능
+// // let: 재선언 불가능, 재할당 가능 <- var보다는 사용권장
+// //const: 재선언 불가능, 재할당 불가능
+
+// const variable = 1; //상수
+
+// let names = ["홍길동","성춘향","짱구","흰둥이"]
+// console.log(names)
+// names.push("짱아")
+// console.log(names)
+
+// names.pop()
+// console.log(names)
+
+// names.unshift("신형만")//배열 제일 앞에 값을 추가하는 기능
+// console.log(names)
+
+// names.shift()//배열의 제일 앞값 삭제
+// console.log(names)
+
+// let index = names.indexOf("짱구")//대상 인덱스 값 반환 (값이 없을 경우 -1)
+// console.log(index)
+
+// let isIncludes = names.includes("짱아")// 값이 포함되어 있는지 반환 boolean형
+// console.log(isIncludes)
+
+// let rainbow = ["빨","주","노","초","파","남","보"]
+// console.log(rainbow[2])
+// rainbow.push("하늘")
+// console.log(rainbow)
+// console.log(rainbow.indexOf("black"))
+// console.log(rainbow.reverse())
